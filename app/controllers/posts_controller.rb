9b2885d7eae7c93a69d30f553f9422c5d12e7be8
ya-sho <78021007+ya-sho@users.noreply.github.com>
2021-02-16 11:13:08 +0900

    メモ投稿機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
     @posts = Post.all.order(id: "DESC")
   end
   def create
-    Post.create(content: params[:content],checked:false)
+    post=Post.create(content: params[:content], checked: false)
     render json:{post:post}
   end
   def checked
